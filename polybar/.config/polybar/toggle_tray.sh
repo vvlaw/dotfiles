9003025fa9d7d2274a8733663c3a6cfb42f440b4
vvlaw <vvtreo@gmail.com>
2026-03-16 20:11:12 +1100

    Sync i3 config: 2026-03-16 20:11:12

--- polybar/.config/polybar/toggle_tray.sh
+++ polybar/.config/polybar/toggle_tray.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-# 1. 优雅关闭并静默报错
-killall -q -15 polybar >/dev/null 2>&1
+killall -q -15 polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
-# 2. 等待旧进程完全释放 Socket
-while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
+sleep 0.3  # 给 i3 一点反应时间
 
-# 3. 切换逻辑
 if [ -f "/tmp/polybar_is_full" ]; then
     rm "/tmp/polybar_is_full"
-    # 后台运行并静默所有输出
     polybar main >/dev/null 2>&1 &
 else
     touch "/tmp/polybar_is_full"
